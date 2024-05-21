aedc114cbb0cbd29ed08a5a7ed245a1ba9633719
lmeyers3840 <lmeyers3840@student.pps.net>
2024-05-21 08:34:01 -0700

    reverted additions of new item and custom creative mode tab because of error. THE GITHUB NIGHTMARE

--- src/main/java/net/levim/dust/Item/ModItems.java
+++ src/main/java/net/levim/dust/Item/ModItems.java
@@ -1,7 +1,6 @@
 package net.levim.dust.Item;
 
 import net.levim.dust.Dust;
-import net.minecraft.world.inventory.ItemCombinerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,8 +11,6 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Dust.MOD_ID);
 
     public static final RegistryObject<Item> DUST = ITEMS.register("dust", () -> new Item(new Item.Properties()));
-
-
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
